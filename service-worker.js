b64e7770162a098aa9c1b6835233f43dacbb4b7b
Tatum Stafford <tatumstafford1@gmail.com>
2021-03-05 15:23:30 -0600

    pwa

--- service-worker.js
+++ service-worker.js
@@ -46,14 +46,14 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url);
+    //console.log('fetch request : ' + e.request.url);
     e.respondWith(
         caches.match(e.request).then(function(request) {
             if (request) { // if cache is available, respond with cache
                 console.log('responding with cache : ' + e.request.url);
                 return request
             } else {
-                console.log('file is not cached, fetching : ' + e.request.url);
+                //console.log('file is not cached, fetching : ' + e.request.url);
                 return fetch(e.request)
             }
         })
